fd20ca70bd3a95476b01a4fecbd90a208df5c82c
Cole Higgins <chiggins@redhat.com>
2020-10-14 13:04:59 -0400

    Pytest Conversion for AK tests

--- pytest_fixtures/api_fixtures.py
+++ pytest_fixtures/api_fixtures.py
@@ -5,7 +5,9 @@ from nailgun import entities
 from wrapanapi import AzureSystem
 from wrapanapi import GoogleCloudSystem
 
+from robottelo import manifests
 from robottelo.api.utils import publish_puppet_module
+from robottelo.api.utils import upload_manifest
 from robottelo.constants import AZURERM_RG_DEFAULT
 from robottelo.constants import AZURERM_RHEL7_FT_BYOS_IMG_URN
 from robottelo.constants import AZURERM_RHEL7_FT_CUSTOM_IMG_URN
@@ -24,6 +26,7 @@ from robottelo.constants import RHEL_7_MAJOR_VERSION
 from robottelo.constants.repos import CUSTOM_PUPPET_REPO
 from robottelo.helpers import download_gce_cert
 from robottelo.test import settings
+
 
 # Global Satellite Entities
 
@@ -44,6 +47,14 @@ def default_location():
 @pytest.fixture(scope='module')
 def module_org():
     return entities.Organization().create()
+
+
+@pytest.fixture(scope='module')
+def module_manifest_org():
+    org = entities.Organization().create()
+    with manifests.clone() as manifest:
+        upload_manifest(org.id, manifest.content)
+    return org
 
 
 @pytest.fixture(scope='module')
@@ -397,6 +408,11 @@ def azurermclient(azurerm_settings):
 
 
 # Katello Entities
+
+
+@pytest.fixture(scope='module')
+def module_activation_key(module_org):
+    return entities.ActivationKey(organization=module_org).create()
 
 
 @pytest.fixture(scope='module')
